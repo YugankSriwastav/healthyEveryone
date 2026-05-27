3a1eecf8c33ded06cfe84d9de1d6b3d9abd3b854
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-05-27 16:55:34 +0530

    Intital Commit Project Setup Day 1st

---
+++ src/main/java/shiva_care/healthify/HealthyFiyApplication.java
@@ -0,0 +1,13 @@
+package shiva_care.healthify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyFiyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyFiyApplication.class, args);
+	}
+
+}
